0f2afc2374c60eea7a719e857f26ae4e3cbf3057
analuisadelmt-dev <analuisa.delmt@gmail.com>
2026-03-17 19:26:28 -0400

    Create consultas.sql

---
+++ consultas.sql
@@ -0,0 +1,11 @@
+SELECT * FROM funcionarios;
+
+SELECT nome, salario FROM funcionarios;
+
+SELECT * 
+FROM funcionarios
+WHERE departamento = 'TI';
+
+SELECT * 
+FROM funcionarios
+WHERE data_demissao IS NULL;
